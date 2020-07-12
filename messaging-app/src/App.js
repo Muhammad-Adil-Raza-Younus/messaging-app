a7d2bc1a7536a23a86214523b64f8632aba37793
devgeorge <thirteenpluralsight@gmail.com>
2020-07-12 14:54:54 +0800

    Initial commit of the messaging app

---
+++ messaging-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import VideoConference from './components/VideoConference';
+
+function App() {
+  return (
+    <div className="App">
+      <VideoConference></VideoConference>
+    </div>
+  );
+}
+
+export default App;
